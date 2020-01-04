66b1dfbc4d7b4e25a03f78d32ff2670ab5dd904f
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2020-01-03 23:22:53 -0600

    Added Multiplication and Division functionality

--- longmath/source/main.cpp
+++ longmath/source/main.cpp
@@ -32,6 +32,31 @@ LUA_FUNCTION(vaas_lm_subtract){
 	return 1;
 }
 
+LUA_FUNCTION(vaas_lm_multiply){
+	LUA->CheckType(1,GarrysMod::Lua::Type::STRING);
+	LUA->CheckType(2,GarrysMod::Lua::Type::STRING);
+	__int64 SID = std::stoull(LUA->GetString(1));
+	__int64 SID_2 = std::stoull(LUA->GetString(2));
+	__int64 Out = SID * SID_2;
+	if(!Out || Out <= 0){
+		LUA->Push(false);
+	}
+	LUA->PushString(std::to_string(Out).c_str());
+	return 1;
+}
+
+LUA_FUNCTION(vaas_lm_divide){
+	LUA->CheckType(1,GarrysMod::Lua::Type::STRING);
+	LUA->CheckType(2,GarrysMod::Lua::Type::STRING);
+	__int64 SID = std::stoull(LUA->GetString(1));
+	__int64 SID_2 = std::stoull(LUA->GetString(2));
+	__int64 Out = SID / SID_2;
+	if(!Out || Out <= 0){
+		LUA->Push(false);
+	}
+	LUA->PushString(std::to_string(Out).c_str());
+	return 1;
+}
 
 void Vaas_LongMath_init(GarrysMod::Lua::ILuaBase* LUA){
 	printMessage(LUA,"VaasDRM module loaded.\n",0,255,0);
@@ -40,6 +65,10 @@ void Vaas_LongMath_init(GarrysMod::Lua::ILuaBase* LUA){
 		LUA->SetField(-2,"longmath_add");
 		LUA->PushCFunction(vaas_lm_subtract);
 		LUA->SetField(-2,"longmath_subtract");
+		LUA->PushCFunction(vaas_lm_multiply);
+		LUA->SetField(-2, "longmath_multiply");
+		LUA->PushCFunction(vaas_lm_divide);
+		LUA->SetField(-2, "longmath_divide");
 	LUA->Pop();
 }
 
